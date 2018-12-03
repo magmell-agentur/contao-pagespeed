a1d5e96af44d9d2e461830e4d6f0cb722c323e76
Markus Milkereit <markus.milkereit@magmell.de>
2018-12-03 22:33:27 +0100

    bugfix: undefinied array notice/warning (after disabling exceptions)

--- src/Hooks/FrontendTemplateHook.php
+++ src/Hooks/FrontendTemplateHook.php
@@ -20,6 +20,7 @@ class FrontendTemplateHook
             $strHeadContent .= "<script>!function(e,t,r){function n(){for(;d[0]&&\"loaded\"==d[0][f];)c=d.shift(),c[o]=!i.parentNode.insertBefore(c,i)}for(var s,a,c,d=[],i=e.scripts[0],o=\"onreadystatechange\",f=\"readyState\";s=r.shift();)a=e.createElement(t),\"async\"in i?(a.async=!1,e.head.appendChild(a)):i[f]?(d.push(a),a[o]=n):e.write(\"<\"+t+' src=\"'+s+'\" defer></'+t+\">\"),a.src=s}(document,\"script\",[";
 
             // exceptions that have nice async rules and handling already
+			$arrExceptions = array();
             // $arrExceptions = ["https://maps.googleapis.com/maps/api/js", "https://maps.google.com/maps/api"];
 
             foreach ($GLOBALS['TL_JAVASCRIPT'] as $strScriptFile) {
